b1000c7292d6f989ac5da8e16005e3d9aea0e19e
rajeev kumar <mca.rajeev@yahoo.in>
2015-11-09 10:44:39 +0530

    1

---
+++ Test2/src/main/java/com/example/maven/jaxws/helloservice/Hello.java
@@ -0,0 +1,17 @@
+package com.example.maven.jaxws.helloservice;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public class Hello {
+	private final String message = "Hello, ";
+
+	public Hello() {
+	}
+
+	@WebMethod
+	public String sayHello(String name) {
+		return message + name + ".";
+	}
+}
